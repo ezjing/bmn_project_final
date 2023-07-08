15a35c137e3b3f20dd8fdb2a3f09e572b058cc17
DESKTOP-OAU9JF5\PC <jeti11@naver.com>
2023-07-08 22:31:37 +0900

    2023-07-08 집/ 문의게시판 페이징 구현 및 가게정보 ui 수정

--- src/main/java/com/bitc/bmn_project/controller/BaeController.java
+++ src/main/java/com/bitc/bmn_project/controller/BaeController.java
@@ -69,59 +69,6 @@ public class BaeController {
     return mv;
   }
 
-// 스크롤 저장 후 불러오기로 시도한 것
-
-//  @RequestMapping(value = "/pageControl/{ceoIdx}", method = RequestMethod.GET)
-//  public ModelAndView pageControll(@PathVariable("ceoIdx") int ceoIdx, HttpServletRequest req, @RequestParam(required = false, defaultValue = "1") int pageNum, HttpServletResponse resp) throws Exception {
-//    ModelAndView mv = new ModelAndView("bmn/viewDetail");
-//
-//    HttpSession session = req.getSession();
-//
-//    // 가게정보 조회
-//    CeoDTO ceoDto = baeService.selectCeoDetail(ceoIdx);
-//
-//    // 팔로워수 조회
-//    String ceoStore = ceoDto.getCeoStore();
-//    int followCnt = baeService.getFollows(ceoStore);
-//
-//    // 팔로워 수 ceoTp 연동
-//    baeService.updateCeoTpFollows(followCnt, ceoIdx);
-//
-//    // 총 리뷰수 조회
-//    int reviewCnt = baeService.getReviewCnt(ceoIdx);
-//
-//    // 리뷰정보 조회
-//    List<ReviewDTO> reviewList = baeService.selectReviewList(ceoIdx);
-//
-//
-//    // 문의 게시판(페이징 처리)
-//    PageInfo<QuestionDTO> questionList = new PageInfo<>(baeService.selectQuestionList(ceoIdx, pageNum), 5);
-//
-//    // 문의 게시판(페이징 전)
-////    List<QuestionDTO> questionList = baeService.selectQuestionList(ceoIdx, pageNum);
-//
-//    session.setAttribute("customerIdx", 3);
-//    session.setAttribute("customerNick", "아이유");
-//
-//    mv.addObject("ceoDto", ceoDto);
-//    mv.addObject("followCnt", followCnt);
-//    mv.addObject("reviewCnt", reviewCnt);
-//    mv.addObject("reviewList", reviewList);
-//    mv.addObject("questionList", questionList);
-//
-//    String scrollY = (String) session.getAttribute("scrollY");
-//    resp.setContentType("text/html; charset=UTF-8");
-//    PrintWriter out = resp.getWriter();
-//
-//    String js = "<script>";
-//    js += "window.scrollTo('" + scrollY + "');";
-//    js += "</script>";
-//
-//    out.println(js);
-//
-//    return mv;
-//  }
-
 
   // 리뷰에 댓글 달기
   @ResponseBody
@@ -181,14 +128,14 @@ public class BaeController {
 //    return "redirect:/bmn/viewDetail/" + questionDTO.getCeoIdx();
   }
 
-  // 페이징 처리를 위한 PageInfo 타입 리스트 가져오기
-//  @ResponseBody
-//  @RequestMapping(value = "/viewDetail/{ceoIdx}", method = RequestMethod.POST)
-//  public Object questionPageControll(@PathVariable("ceoIdx") int ceoIdx, @RequestParam(required = false, defaultValue = "1") int pageNum) throws Exception {
-//    PageInfo<QuestionDTO> questionList = new PageInfo<>(baeService.selectQuestionList(ceoIdx, pageNum), 5);
-//
-//    return questionList;
-//  }
+//   페이징 처리를 위한 PageInfo 타입 리스트 가져오기
+  @ResponseBody
+  @RequestMapping(value = "/bmn/questionPaging", method = RequestMethod.GET)
+  public Object questionPageControl(@RequestParam("ceoIdx") int ceoIdx, @RequestParam(required = false, defaultValue = "1") int pageNum) throws Exception {
+    PageInfo<QuestionDTO> questionList = new PageInfo<>(baeService.selectQuestionList(ceoIdx, pageNum), 5);
+
+    return questionList;
+  }
   
 }
 
